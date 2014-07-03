cd431293811715ccb92f79857dd7feb37abefe2a
gefestvg <gefest_v@mail.ru>
2014-07-03 17:48:24 +0300

    Update jsmarty.js
    modifier transliterate and replace spaces

--- jsmarty.js
+++ jsmarty.js
@@ -3359,6 +3359,32 @@
         var tpl = new jSmart(this);
         return tpl.fetch(data);
     };
+    
+//modifier transliterate and replace spaces
+    jSmart.prototype.registerPlugin(
+        'modifier',
+        'translit_and_replace_spaces',
+        function(s)
+        {
+            var tr = {
+            "А" : "a", "Б" : "b", "В" : "v", "Г" : "g",
+            "Д" : "d", "Е" : "e", "Ж" : "j", "З" : "z", "И" : "i",
+            "Й" : "y", "К" : "k", "Л" : "l", "М" : "m", "Н" : "n",
+            "О" : "o", "П" : "p", "Р" : "r", "С" : "s", "Т" : "t",
+            "У" : "u", "Ф" : "f", "Х" : "h", "Ц" : "ts", "Ч" : "ch",
+            "Ш" : "sh", "Щ" : "sch", "Ъ" : "", "Ы" : "yi", "Ь" : "",
+            "Э" : "e", "Ю" : "yu", "Я" : "ya", "а" : "a", "б" : "b",
+            "в" : "v", "г" : "g", "д" : "d", "е" : "e", "ж" : "j",
+            "з" : "z", "и" : "i", "й" : "y", "к" : "k", "л" : "l",
+            "м" : "m", "н" : "n", "о" : "o", "п" : "p", "р" : "r",
+            "с" : "s", "т" : "t", "у" : "u", "ф" : "f", "х" : "h",
+            "ц" : "ts", "і" :"i", "І":"I", "ч" : "ch", "ш" : "sh",
+            "щ" : "sch", "ъ" : "y", "ї" : "yi", "Ї" : "Yi", "є" : "ye", "Є":"Ye",
+            "ы" : "yi", "ь" : "", "э" : "e", "ю" : "yu", "я" : "ya",
+            " " : " ", "." : ".", "/" : "_"};
+            return jSmart.prototype.PHPJS('strtr','translit_and_replace_spaces').strtr(jSmart.prototype.PHPJS('str_replace','translit_and_replace_spaces').str_replace([' ', '.', ','], ['_','',''], s.toLowerCase()),tr);
+        }
+    );
 
 })()
 
